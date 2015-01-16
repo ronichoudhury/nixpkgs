815956def63325dd6e9adcb82f248368d4264877
Roni Choudhury <roni.choudhury@kitware.com>
2015-01-15 19:22:13 -0500

    WIP - working on tangelo-git derivation

--- tangelo-git/builder.sh
+++ tangelo-git/builder.sh
@@ -1,11 +1,16 @@
 source $stdenv/setup
 
-PATH=$grunt/bin:$npm/bin:$PATH
+PATH=$grunt/bin:$npm/bin:$git/bin:$nodejs/bin:$PATH
 
+# Create a temp directory where we have write permission, and copy the sources
+# there.
 mkdir tmp
 cp -a $src/* tmp
-ls -l -R
+chmod -R u+w tmp
+
 cd tmp
-grunt clean:all || npm install
-npm install
-grunt package
+grunt clean:all --force
+#HOME=. npm install
+
+mkdir $out
+cp setup.cfg Gruntfile.js $out
